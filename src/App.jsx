a76712588da4566dd2990fe438906e7e4d8dc1ad
viktor <araujo>
2025-04-17 23:11:09 -0300

    Components imported and included

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import Hero from './components/Hero'
+import About from './components/About'
+import Skills from './components/Skills'
+import Experience from './components/Experience'
+import Education from './components/Education'
+import Contact from './components/Contact'
+import Navbar from './components/Navbar'
+import ScrollToTop from './components/ScrollToTop'
 
 export default function App() {
   return (
     <>
+      <Navbar />
       <Hero />
+      <About />
+      <Skills />
+      <Experience />
+      <Education />
+      <Contact />
+      <ScrollToTop />
     </>
   );
 }
