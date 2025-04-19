affcc5c6f50310960c1d2aacd1a0a4485e0412c1
viktor <araujo>
2025-04-19 00:00:26 -0300

    Vitest setup

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import vitest from 'eslint-plugin-vitest';
 
 export default [
   { ignores: ['dist'] },
@@ -28,6 +29,16 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+    },
+  },
+  {
+    files: ['**/*.test.{js,jsx}'],
+    plugins: { vitest },
+    languageOptions: {
+      globals: globals.vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
     },
   },
 ]
